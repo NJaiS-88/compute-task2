498034abd672d855b5aa59c805d27455952061fd
Jai Sumanth Nekkanti <jaisumanthnekkanti@gmail.com>
2025-10-18 15:40:45 +0530

    form created

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react"
+import TodoForm from "./components/TodoForm"
 function App() {
+  const [id, setId] = useState(1);
+  const [task, setTask] =useState([]);
+
+  const adding = (a) => {
+    const obj = {"id": id, "task":task, "completed": false}
+  }
   return (
     <>
-      
+      <TodoForm></TodoForm>
     </>
   )
 }
---
+++ to-do-list/src/components/TodoForm.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+function TodoForm({adding}){
+    const [taskData, setTaskData] = useState("")
+    const [error, showError] = useState("")
+
+    const changeTaskData = (e) => {
+        setTaskData(e.target.value);
+    }
+
+    const submitfunc = (e) => {
+        e.preventDefault();
+        if(taskData.trim()){
+            adding(taskData.trim());
+            setTaskData("");
+        }else{
+            showError("task cant be empty!");
+        }
+    }
+
+    return( <>
+                <div>
+                    <form onSubmit={submitfunc}>
+                        <input onChange={changeTaskData} value={taskData} placeholder="click here to enter your task" 
+                            className="border-none focus:outline-none focus:border-none w-[]"></input>
+                        <div>{taskData}</div>
+                        <button type="submit">Add Task</button>
+                        <div>{error}</div>
+                    </form>
+                </div>
+            </>)
+}
+export default TodoForm;
