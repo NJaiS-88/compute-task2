6a84be376dcf8b54318e66e31cb862d805652594
Jai Sumanth Nekkanti <jaisumanthnekkanti@gmail.com>
2025-10-19 16:41:12 +0530

    logic completed

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -15,6 +15,10 @@ function App() {
 
   const length = task.length;
 
+  const [active, setActive] = useState(false);
+  const [all, setAll] = useState(true);
+  const [completed, setCompleted] = useState(false);
+
   useEffect(() => {
     localStorage.setItem("task", JSON.stringify(task))
     const saved = localStorage.getItem("task");
@@ -22,13 +26,13 @@ function App() {
     const maxId = a.length ? Math.max(...a.map(t => t.id)) : 0;
     setId(maxId+1)
     
-  }, [task]) 
+  }, [task]);
 
   const adding = (a) => {
     const obj = {"id":id, "task":a, "completed": false}
     setTask([obj,... task])
     setId(i => i+=1);
-  }
+  };
 
   const complete = (a) => {
     const newlist = task.map(t => {
@@ -41,12 +45,12 @@ function App() {
     const b = newlist.splice(i,1)[0];
     newlist.push(b);
     setTask(newlist);
-  }
+  };
 
   const deletion = (a) => {
     const newList = task.filter(t => t.id !== a);
     setTask(newList);
-  }
+  };
 
   const up = (a) => {
     const i = task.findIndex(t => t.id===a);
@@ -70,11 +74,26 @@ function App() {
     }
   }
 
+  let filteredTasks = [... task];
+
+  if(active){
+    filteredTasks = task.filter(t => !t.completed);
+  }else if (completed){
+    filteredTasks = task.filter(t => t.completed);
+  }
+
   return (
-    <>
+    <div className="max-w-[100vw] h-[100vh] overflow-hidden relative bg-blue-500">
+      <div className="md:text-[4vw] sm:text-[2rem] text-[1rem] md:top-[3%] sm:top-[4%] top-[5%] text-white absolute left-1/2 transform -translate-x-1/2">TO DO LIST</div>
+      <div>
+        <div onClick={() => {setAll(true), setActive(false), setCompleted(false)}} className={`${all? "bg-blue-500":""}`}>All</div>
+        <div onClick={() => {setAll(false), setActive(true), setCompleted(false)}} className={`${active? "bg-blue-500":""}`}>Active</div>
+        <div onClick={() => {setAll(false), setActive(false), setCompleted(true)}} className={`${completed? "bg-blue-500":""}`}>Completed</div>
+      </div>
+      <div className="h-[80vh] w-[100vw] rounded-tl-[3vw] rounded-tr-[3vw] absolute bottom-0 bg-gray-200"></div>
       <TodoForm adding={adding}></TodoForm>
-      <TodoList task={task} complete={complete} deletion={deletion} up={up} down={down} length={length}></TodoList>
-    </>
+      <TodoList task={filteredTasks} complete={complete} deletion={deletion} up={up} down={down} length={length}></TodoList>
+    </div>
   )
 }
 
--- to-do-list/src/components/TodoItem.jsx
+++ to-do-list/src/components/TodoItem.jsx
@@ -1,8 +1,8 @@
-function TodoItem({t, complete, deletion, up, down, length, index}){
+function TodoItem({t, complete, deletion, up, down, length, index, active}){
     return( <>
                 <li>
                     <div className={`${t.completed? "line-through":""}`}>{t.task}</div>
-                    <button onClick={() => complete(t.id)}>Complete</button>
+                    <button onClick={() => complete(t.id)} className={`${t.completed? "hidden":"visible"}`}>Complete</button>
                     <button onClick={() => deletion(t.id)}>Delete</button>
                     <button onClick={() => up(t.id)} className={`${index<1 || t.completed? "hidden":"visible"}`}>up</button>
                     <button onClick={() => down(t.id)} className={`${index===length-1 || t.completed? "hidden":"visible"}`}>down</button>
