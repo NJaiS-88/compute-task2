f4ef7d2ebc997e2a105914c2f43d27ca822139f9
Jai Sumanth Nekkanti <jaisumanthnekkanti@gmail.com>
2025-10-20 10:56:33 +0530

    added icons

--- to-do-list/src/components/TodoItem.jsx
+++ to-do-list/src/components/TodoItem.jsx
@@ -1,13 +1,17 @@
+import { MdDeleteForever } from "react-icons/md";
+import { FaCheckCircle } from "react-icons/fa";
+import { FaArrowUp } from "react-icons/fa";
+import { FaArrowDown } from "react-icons/fa6";
 function TodoItem({t, complete, deletion, up, down, length, index, active}){
     return( <>
                 <li>
-                    <div className="md:w-[30rem] w-[40vw]  my-4 py-3 shadow-sm justify-between  bg-white px-3 rounded-lg h-fit flex flex-row flex-wrap">
+                    <div className={`md:w-[30rem] w-[40vw]  my-4 py-3 shadow-sm justify-between ${t.completed? "bg-blue-50":"bg-white"} px-3 rounded-lg h-fit flex flex-row flex-wrap"`}>
                         <div className={`md:w-[40%] w-[100%] break-words px-4 h-fit ${t.completed? "line-through":""}`}>{t.task}</div>
                         <div className="">
-                            <button onClick={() => complete(t.id)} className={` text-white rounded-lg w-fit h-fit p-1 bg-blue-300 cursor-pointer mx-1 ${t.completed? "hidden":"visible"}`}>Complete</button>
-                            <button onClick={() => deletion(t.id)}className="text-white rounded-lg w-fit h-fit p-1 bg-blue-300 mx-1 cursor-pointer">Delete</button>
-                            <button onClick={() => up(t.id)} className={`text-white rounded-lg w-fit h-fit p-1 bg-blue-300 mx-1 cursor-pointer ${index<1 || t.completed? "hidden":"visible"}`}>up</button>
-                            <button onClick={() => down(t.id)} className={`text-white rounded-lg w-fit h-fit p-1 bg-blue-300 mx-1 cursor-pointer ${index===length-1 || t.completed? "hidden":"visible"}`}>down</button>
+                            <button onClick={() => complete(t.id)} className={`active:bg-blue-100 hover:bg-blue-500 text-white rounded-lg text-xl w-fit h-fit p-1 bg-blue-300 cursor-pointer mx-1 ${t.completed? "hidden":"visible"}`}><FaCheckCircle></FaCheckCircle></button>
+                            <button onClick={() => deletion(t.id)}className=" active:bg-blue-100 hover:bg-blue-500 text-white rounded-lg text-xl w-fit h-fit p-1 bg-blue-300 mx-1 cursor-pointer"><MdDeleteForever></MdDeleteForever></button>
+                            <button onClick={() => up(t.id)} className={`active:bg-blue-100 hover:bg-blue-500 text-white rounded-lg text-xl w-fit h-fit p-1 bg-blue-300 mx-1 cursor-pointer ${index<1 || t.completed? "hidden":"visible"}`}><FaArrowUp></FaArrowUp></button>
+                            <button onClick={() => down(t.id)} className={`active:bg-blue-100 hover:bg-blue-500 text-white rounded-lg text-xl w-fit h-fit p-1 bg-blue-300 mx-1 cursor-pointer ${index===length-1 || t.completed? "hidden":"visible"}`}><FaArrowDown></FaArrowDown></button>    
                         </div>                 
                     </div>
                 </li>
